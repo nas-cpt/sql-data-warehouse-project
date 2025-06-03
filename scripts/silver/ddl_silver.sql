0304f6c05e9a7fc51ba96d98f26b28d683d65e8e
nas-cpt <nadjemin.aboubacar.soro@gmail.com>
2025-06-03 12:55:21 +0200

    Update ddl_silver.sql

--- scripts/silver/ddl_silver.sql
+++ scripts/silver/ddl_silver.sql
@@ -46,15 +46,15 @@ if object_id('silver.crm_sales_details' , 'u') is not null
 	drop table silver.crm_sales_details;
 go
 create table silver.crm_sales_details(
-	sls_ord_num         nvarchar(50),
-	sls_prd_key         nvarchar(50),
-	sls_cust_id         int,
-	sls_order_dt        date,
-	sls_ship_dt         date,
-	sls_due_dt          date,
-	sls_sales           int,
-	sls_quantity        int,
-	sls_price           int,
+	sls_ord_num  	          nvarchar(50),
+	sls_prd_key   	          nvarchar(50),
+	sls_cust_id   	          int,
+	sls_order_dt 	          date,
+	sls_ship_dt  	          date,
+	sls_due_dt    	          date,
+	sls_sales     	          int,
+	sls_quantity              int,
+	sls_price         	  int,
 	dwh_create_date		  datetime2 default getdate()
 );
 go
@@ -64,9 +64,9 @@ if object_id('silver.erp_cust_az12' , 'u') is not null
 go
   
 create table silver.erp_cust_az12(
-	cid                 nvarchar(50),
-	bdate               date,
-	gen                 nvarchar(50),
+	cid       	          nvarchar(50),
+	bdate      	          date,
+	gen           	          nvarchar(50),
 	dwh_create_date		  datetime2 default getdate()
 );
 go
@@ -76,8 +76,8 @@ if object_id('silver.erp_loc_a101' , 'u') is not null
 go
   
 create table silver.erp_loc_a101(
-	cid                 nvarchar(50),
-	cntry               nvarchar(50),
+	cid        	          nvarchar(50),
+	cntry         	          nvarchar(50),
 	dwh_create_date		  datetime2 default getdate()
 );
 go
@@ -87,10 +87,10 @@ if object_id('silver.erp_px_cat_g1v2' , 'u') is not null
 go
   
 create table silver.erp_px_cat_g1v2(
-	id                  nvarchar(50),
-	cat                 nvarchar(50),
-	subcat              nvarchar(50),
-	maintenance         nvarchar(50),
+	id        	          nvarchar(50),
+	cat         	          nvarchar(50),
+	subcat       	          nvarchar(50),
+	maintenance   	          nvarchar(50),
 	dwh_create_date		  datetime2 default getdate()
 );
 go
