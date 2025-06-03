611c1bbc296fd80500fecc9447f8e06224f1cd77
nas-cpt <nadjemin.aboubacar.soro@gmail.com>
2025-06-03 12:53:15 +0200

    Update ddl_silver.sql

--- scripts/silver/ddl_silver.sql
+++ scripts/silver/ddl_silver.sql
@@ -30,15 +30,15 @@ if object_id('silver.crm_prd_info' , 'u') is not null
 go
   
 create table silver.crm_prd_info(
-	prd_id              int,
-	cat_id 	     	nvarchar(50),
-	prd_key             nvarchar(50),
-	prd_nm              nvarchar(50),
-	prd_cost            int,
-	prd_line            nvarchar(50),
-	prd_start_dt        date,
-	prd_end_dt          date,
-	dwh_create_date	  	datetime2 default getdate()
+	prd_id                  int,
+	cat_id 	        	nvarchar(50),
+	prd_key    	        nvarchar(50),
+	prd_nm                  nvarchar(50),
+	prd_cost 	        int,
+	prd_line    	        nvarchar(50),
+	prd_start_dt	        date,
+	prd_end_dt   	        date,
+	dwh_create_date	       datetime2 default getdate()
 );
 go
 
